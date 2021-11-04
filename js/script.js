901d71abc0951c4b19ed2e7eb37284cb811d0681
Dani9898 <danieleoliva98@gmail.com>
2021-11-04 16:08:18 +0100

    es finito

--- js/script.js
+++ js/script.js
@@ -5,29 +5,29 @@ const membri = [
         "foto": "img/wayne-barnett-founder-ceo.jpg", 
     },
     {
-        "nome": "Wayne Barnett",
-        "ruolo": "Founder & CEO",
-        "foto": "img/wayne-barnett-founder-ceo.jpg", 
+        "nome": "Angela Caroll",
+        "ruolo": "Chied Editor",
+        "foto": "img/angela-caroll-chief-editor.jpg", 
     },
     {
-        "nome": "Wayne Barnett",
-        "ruolo": "Founder & CEO",
-        "foto": "img/wayne-barnett-founder-ceo.jpg", 
+        "nome": "Angela Lopez",
+        "ruolo": "Social Media Manager",
+        "foto": "img/angela-lopez-social-media-manager.jpg", 
     },
     {
-        "nome": "Wayne Barnett",
-        "ruolo": "Founder & CEO",
-        "foto": "img/wayne-barnett-founder-ceo.jpg", 
+        "nome": "Barbara Ramos",
+        "ruolo": "Graphic Designer",
+        "foto": "img/barbara-ramos-graphic-designer.jpg", 
     },
     {
-        "nome": "Wayne Barnett",
-        "ruolo": "Founder & CEO",
-        "foto": "img/wayne-barnett-founder-ceo.jpg", 
+        "nome": "Scott Estrada",
+        "ruolo": "Developer",
+        "foto": "img/scott-estrada-developer.jpg", 
     },
     {
-        "nome": "Wayne Barnett",
-        "ruolo": "Founder & CEO",
-        "foto": "img/wayne-barnett-founder-ceo.jpg", 
+        "nome": "Walter Gordon",
+        "ruolo": "Office Manager",
+        "foto": "img/walter-gordon-office-manager.jpg", 
     },
 
 ];
@@ -66,4 +66,37 @@ console.log(membri);
            </div>
     </div> `
     }
+
+    // al click del pulsante salvare i dati scritti in variabili.
+    const addButton = document.getElementById("addMemberButton");
+    addButton.addEventListener("click", function(){
+
+        let userName = document.getElementById("name").value;
+        let userRole = document.getElementById("role").value;
+        let userImage = document.getElementById("image").value;
+        
+        console.log(userName, userRole, userImage);
+
+        const datiNuovoMembro = {
+            "nome": userName,
+            "ruolo": userRole,
+            "foto": userImage,
+        }
+
+        container.innerHTML += `
+        <div class="team-card">
+             <div class="card-image">
+                 <img
+                  src="${datiNuovoMembro.foto}"
+                  alt="${datiNuovoMembro.nome}"/>
+              </div>
+              <div class="card-text">
+                <h3>${datiNuovoMembro.nome}</h3>
+                <p>${datiNuovoMembro.ruolo}</p>
+               </div>
+        </div> `
+
+    })
+    
+
 
